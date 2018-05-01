f26dabbff2b011e1b55feea5c6d95feb925a7351
Crystal <penalosa619@gmail.com>
2018-05-01 18:09:53 -0400

    Update park-magnetometer.py

--- park-magnetometer.py
+++ park-magnetometer.py
@@ -5,15 +5,19 @@
 # License: Public Domain
 import time
 import RPi.GPIO as GPIO
-
-# Import the LSM303 module.
+import os
+import json
+import geojson
+import time
+import threading
+import urllib
+import urllib2
+import requests
 import Adafruit_LSM303
 
-
-# Create a LSM303 instance.
 lsm303 = Adafruit_LSM303.LSM303()
 
-# Turn on a LED on GPIO 26
+# LED Indicator on GPIO 26
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(26,GPIO.OUT)
@@ -31,19 +35,23 @@ while True:
     print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
           accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
     # Wait 5 seconds and repeat.
-    if mag_z > -100: GPIO.output(26,GPIO.HIGH), status = 'open'
-    else: GPIO.output(26,GPIO.LOW), status = 'closed'
+    if mag_z > -130: 
+        GPIO.output(26,GPIO.LOW) 
+        status = 'open'
+    else: 
+        GPIO.output(26,GPIO.HIGH) 
+        status = 'closed'
 
     #Stae Initialize POST
-    id = 'pkst1'
-    stallNumber = '10'
-    parkingStructure = 'Grand Central Tech Parking Garage'
+    id = 'pkst2'
+    stallNumber = '7'
+    parkingStructure = 'Samsung NEXT Parking Garage'
     type = 'compact'
-    location = geojson.Point((-73.97782176733017,40.753421860439836))
+    location = geojson.Point((-73.99048715829849,40.744050339871116))
     url = 'https://municipal.systems/v1/data?key=5802877b-5901-45c0-9ca2-d4301bc7301f'
     payload = {'status':status, 'stallNumber':stallNumber, 'parkingStructure':parkingStructure, 'type':type, 'location':location, 'id':id}
     r = requests.post(url, json=payload) 
         # params='response=false'
-    print = r.content
+    print r.content
     time.sleep(10)
     
